1aaaba371e13d7741ed8b554867e4fa7a1084d2a
Christopher Arnold <chrisarnold2010@gmail.com>
2018-07-08 23:42:36 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,9 +3,7 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  array.each do |i|
-    puts i.
-  end
+  array.collect { |x| x.capitalize<<"!"
 end
 
 def long_planeteer_calls# code an argument here
