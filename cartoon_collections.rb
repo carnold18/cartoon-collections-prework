dbb9fca52359878d938d5346bf801503002d261b
Christopher Arnold <chrisarnold2010@gmail.com>
2018-07-09 00:01:55 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,5 +15,8 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  cheese_types = ["cheddar", "gouda", "camembert"]
+  if cheese = array.find { |type| type.include? "cheddar" || "gouda" || "camembert"} == true
+    return cheese
+  else
+    return nil
 end
